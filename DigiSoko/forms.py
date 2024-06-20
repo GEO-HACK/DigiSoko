c9e7fb4ef73eca87c50757f4abb1a1f635e2ba86
GEO-HACK <geoffreykirumba0@gmail.com>
2024-06-20 22:08:25 +0300

    footer addition

--- DigiSoko/forms.py
+++ DigiSoko/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from .models import *
 
@@ -5,3 +6,9 @@ class ProductForm(ModelForm):
     class Meta:
         model = Products
         fields = '__all__'
+        widgets = {
+            'product_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'product_description': forms.Textarea(attrs={'class': 'form-control'}),
+            'product_price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'product_image': forms.FileInput(attrs={'class': 'form-control'}),
+        }
